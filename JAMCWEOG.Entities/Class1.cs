ebbe95319c7a5ce7f24dc5a2c32a78fde5978fe1
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-06 23:15:49 -0600

    chore(Entities): crear proyecto de entidades del sistema - José Ángel Mate Carías

---
+++ JAMCWEOG.Entities/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace JAMCWEOG.Entities
+{
+    public class Class1
+    {
+
+    }
+}
--- JAMCWEOG.Entities/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JAMCWEOG.Entities.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
