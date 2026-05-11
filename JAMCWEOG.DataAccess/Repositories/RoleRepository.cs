8e97380e2dda0e9777ba4f5fc4b0991ae7209282
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 17:42:33 -0600

    feat(Repository): agregar RoleRepository para operaciones CRUD - José Ángel Mate Carías

---
+++ JAMCWEOG.DataAccess/Repositories/RoleRepository.cs
@@ -0,0 +1,54 @@
+﻿using JAMCWEOG.DataAccess.Context;
+using JAMCWEOG.Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JAMCWEOG.DataAccess.Repositories
+{
+    public class RoleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // LISTAR
+        public async Task<List<Role>> GetAllAsync()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        // OBTENER POR ID
+        public async Task<Role?> GetByIdAsync(int id)
+        {
+            return await _context.Roles.FindAsync(id);
+        }
+
+        // CREAR
+        public async Task AddAsync(Role role)
+        {
+            await _context.Roles.AddAsync(role);
+            await _context.SaveChangesAsync();
+        }
+
+        // EDITAR
+        public async Task UpdateAsync(Role role)
+        {
+            _context.Roles.Update(role);
+            await _context.SaveChangesAsync();
+        }
+
+        // ELIMINAR
+        public async Task DeleteAsync(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+
+            if (role != null)
+            {
+                _context.Roles.Remove(role);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
