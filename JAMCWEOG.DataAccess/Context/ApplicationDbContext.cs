19bed2cc24b885ae3692da88170f9b1ce83e1aa5
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 17:03:12 -0600

    feat(DAL): agregar ApplicationDbContext para Entity Framework Core - José Ángel Mate Carías

---
+++ JAMCWEOG.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JAMCWEOG.Entities.Entities;
+
+namespace JAMCWEOG.DAL.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
