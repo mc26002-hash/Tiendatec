6c4cc6f5a3cd2caf4d7fd80d6e483e03a4f75422
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 17:56:29 -0600

    feat(Service): agregar ManufacturerService para lógica de negocio de fabricantes - José Ángel Mate Carías

---
+++ JAMCWEOG.BusinessLogic/Services/ManufacturerService.cs
@@ -0,0 +1,45 @@
+﻿using JAMCWEOG.DataAccess.Repositories;
+using JAMCWEOG.Entities.Entities;
+
+namespace JAMCWEOG.BusinessLogic.Services
+{
+    public class ManufacturerService
+    {
+        private readonly ManufacturerRepository _manufacturerRepository;
+
+        public ManufacturerService(ManufacturerRepository manufacturerRepository)
+        {
+            _manufacturerRepository = manufacturerRepository;
+        }
+
+        // LISTAR
+        public async Task<List<Manufacturer>> GetAllAsync()
+        {
+            return await _manufacturerRepository.GetAllAsync();
+        }
+
+        // OBTENER POR ID
+        public async Task<Manufacturer?> GetByIdAsync(int id)
+        {
+            return await _manufacturerRepository.GetByIdAsync(id);
+        }
+
+        // CREAR
+        public async Task AddAsync(Manufacturer manufacturer)
+        {
+            await _manufacturerRepository.AddAsync(manufacturer);
+        }
+
+        // EDITAR
+        public async Task UpdateAsync(Manufacturer manufacturer)
+        {
+            await _manufacturerRepository.UpdateAsync(manufacturer);
+        }
+
+        // ELIMINAR
+        public async Task DeleteAsync(int id)
+        {
+            await _manufacturerRepository.DeleteAsync(id);
+        }
+    }
+}
