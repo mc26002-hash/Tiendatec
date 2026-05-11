aa98ccc3c7f570b4ec0f6abe499b295d7cd9ee0b
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 17:55:31 -0600

    feat(Service): agregar UserService para lógica de negocio de usuarios - José Ángel Mate Carías

---
+++ JAMCWEOG.BusinessLogic/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using JAMCWEOG.DataAccess.Repositories;
+using JAMCWEOG.Entities.Entities;
+
+namespace JAMCWEOG.BusinessLogic.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // LISTAR
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+
+        // OBTENER POR ID
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        // CREAR
+        public async Task AddAsync(User user)
+        {
+            await _userRepository.AddAsync(user);
+        }
+
+        // EDITAR
+        public async Task UpdateAsync(User user)
+        {
+            await _userRepository.UpdateAsync(user);
+        }
+
+        // ELIMINAR
+        public async Task DeleteAsync(int id)
+        {
+            await _userRepository.DeleteAsync(id);
+        }
+    }
+}
