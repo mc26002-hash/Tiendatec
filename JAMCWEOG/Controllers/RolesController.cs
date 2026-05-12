908e180b756f16e166155386333aa96114bc281e
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 18:03:02 -0600

    feat(Controller): agregar RolesController con operaciones CRUD - José Ángel Mate Carías

---
+++ JAMCWEOG/Controllers/RolesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JAMCWEOG.BusinessLogic.Services;
+using JAMCWEOG.Entities.Entities;
+
+namespace JAMCWEOG.WebApplication.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleService _roleService;
+
+        public RolesController(RoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        // LISTAR
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleService.GetAllAsync();
+            return View(roles);
+        }
+
+        // GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                await _roleService.AddAsync(role);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(role);
+        }
+
+        // GET EDIT
+        public async Task<IActionResult> Edit(int id)
+        {
+            var role = await _roleService.GetByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                await _roleService.UpdateAsync(role);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(role);
+        }
+
+        // GET DELETE
+        public async Task<IActionResult> Delete(int id)
+        {
+            var role = await _roleService.GetByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _roleService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DETAILS
+        public async Task<IActionResult> Details(int id)
+        {
+            var role = await _roleService.GetByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+    }
+}
