0196e385cd75f394a6be0d7dace5be62f24ef201
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 19:03:11 -0600

    feat(Controller): agregar UsersController con operaciones CRUD - José Ángel Mate Carías

---
+++ JAMCWEOG/Controllers/UsersController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JAMCWEOG.BusinessLogic.Services;
+using JAMCWEOG.Entities.Entities;
+
+namespace JAMCWEOG.WebApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // LISTAR
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userService.GetAllAsync();
+            return View(users);
+        }
+
+        // GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userService.AddAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(user);
+        }
+
+        // GET EDIT
+        public async Task<IActionResult> Edit(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userService.UpdateAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(user);
+        }
+
+        // GET DELETE
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _userService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DETAILS
+        public async Task<IActionResult> Details(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+    }
+}
