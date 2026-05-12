dc4851b62bea67bee0910a64382eb0feb53d00f1
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-11 19:05:27 -0600

    feat(Controller): agregar ProductsController con operaciones CRUD - José Ángel Mate Carías

---
+++ JAMCWEOG/Controllers/ProductsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JAMCWEOG.BusinessLogic.Services;
+using JAMCWEOG.Entities.Entities;
+
+namespace JAMCWEOG.WebApplication.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // LISTAR
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+
+        // GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
+        // GET EDIT
+        public async Task<IActionResult> Edit(long id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.UpdateAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
+        // GET DELETE
+        public async Task<IActionResult> Delete(long id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            await _productService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // DETAILS
+        public async Task<IActionResult> Details(long id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
